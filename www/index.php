af5e851c5d643b501a63516e9d606b925a3ab09d
KRAILERKM <krailerk_man@hotmail.com>
2020-03-23 15:49:07 +0700

    03-23-2020-1548 Upload

---
+++ www/index.php
@@ -0,0 +1,10 @@
+<?
+	//
+	//  index.php
+	//  RCMS
+	//
+	//  Created by KRAILERKM on 1/30/55 BE.
+	//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+	//
+	header("Location:./login/logout.php");
+?>
